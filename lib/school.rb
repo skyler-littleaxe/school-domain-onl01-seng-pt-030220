d131215558f0dc9f228aa832b0b23010219fad18
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-23 00:12:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,47 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+
+
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+  end
+  
+  
+  
+  
+  
+  def add_student(student, grade_lvl)
+    if @roster.include?(grade_lvl)
+  @roster[grade_lvl] << student
+    else
+  @roster[grade_lvl] = []
+  @roster[grade_lvl] << student
+  
+    end
+  end
+   
+   def grade(grade_lvl)
+      @roster[grade_lvl]
+   end
+   
+   def sort
+
+   @roster.values.sort!
+   @roster
+  
+    
+    end
+  
+ 
+  
+  
+  
+  #def add_student(student, grade)
+   # @roster << (#grade => #student)
+  #end
+    
+  
+  
+end
