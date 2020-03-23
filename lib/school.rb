42495588b37afc151033a448fcdb0643951fab2a
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-23 00:13:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,23 +13,23 @@ class School
   
   
   def add_student(student, grade_lvl)
-    if @roster.include?(grade_lvl)
-  @roster[grade_lvl] << student
+    if roster.include?(grade_lvl)
+  roster[grade_lvl] << student
     else
-  @roster[grade_lvl] = []
-  @roster[grade_lvl] << student
+  roster[grade_lvl] = []
+  roster[grade_lvl] << student
   
     end
   end
    
    def grade(grade_lvl)
-      @roster[grade_lvl]
+      roster[grade_lvl]
    end
    
    def sort
 
-   @roster.values.sort!
-   @roster
+   roster.values.sort!
+   roster
   
     
     end
